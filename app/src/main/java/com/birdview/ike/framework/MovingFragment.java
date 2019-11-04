dd76bf6aa2fc3a8a88cbf9e13910dc2e4fe4eb31
KO-Run <betelhemtilahun@gmail.com>
2019-11-04 22:47:16 +0300

    menu

--- app/src/main/java/com/birdview/ike/framework/MovingFragment.java
+++ app/src/main/java/com/birdview/ike/framework/MovingFragment.java
@@ -1,6 +1,57 @@
 package com.birdview.ike.framework;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-public class MovingFragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MovingFragment extends Fragment implements View.OnClickListener {
+    private Button house_item;
+    private Button car;
+    private MViewPagerAdapter adapter;
+    public MovingFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.moving_fragment,container,false);
+        house_item = (Button) rootView.findViewById(R.id.house_item);
+        car = (Button) rootView.findViewById(R.id.car);
+
+        house_item.setOnClickListener(this);
+        car.setOnClickListener(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Fragment fragment = null;
+        switch (v.getId()){
+            case R.id.house_item:
+                fragment = new HouseFragment();
+                replaceFragment(fragment);
+                break;
+
+            case R.id.car:
+                fragment = new CarFragment();
+                replaceFragment(fragment);
+                break;
+
+        }
+
+    }
+    public void replaceFragment(Fragment someFragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.veiwpager_id, someFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
